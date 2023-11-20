28ee453d57600164cea669fdd597b60a4cd9cf98
José Antonio <120281596+jPrietoMz@users.noreply.github.com>
2023-11-20 17:13:18 +0100

    Guardar archivos diferentes

--- jvdb.c
+++ jvdb.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
-
+#include <time.h>
+/*
 void updateRecord(const char *filename, const char *oldData, const char *newData) {
     FILE *oldFile = fopen(filename, "r");
     FILE *newFile = fopen("temp.txt", "w");
@@ -53,17 +54,27 @@ void deleteRecord(const char *filename, const char *data) {
 
     printf("Record deleted successfully.\n");
 }
-
+*/
 int main(int argc, char *argv[]){
     FILE *archivo;
     char *operacion=argv[1];
     char *basededatos=argv[2];
-    char *nombrearchivo=argv[3];
+    char *coleccion=argv[3];
+    char *documento=argv[4];
     char ruta[100];
     strcpy(ruta,basededatos);
-    strcat(ruta,"-");
-    strcat(ruta,nombrearchivo);
-    strcat(ruta,".txt");
+    strcat(ruta,"/");
+    strcat(ruta,coleccion);
+    strcat(ruta,"/");
+    strcat(ruta,documento);
+    strcat(ruta,".json");
+    
+    printf(" La ruta es: %s", ruta);
+    
+   /* time_t tiempoactual;
+    tiempoactual = time(NULL);
+    printf("tiempo actual: &lld \n", (long long)tiempoactual);
+    printf("Fin");*/
     
     
     if(strcmp(operacion,"select") == 0){
@@ -75,18 +86,18 @@ int main(int argc, char *argv[]){
             printf("linea: %s",linea);
         }  
     }else if(strcmp(operacion,"insert") == 0){
-        archivo = fopen(ruta,"a");
+        archivo = fopen(ruta,"w");
         char *texto = argv[4];
         fputs(strcat(texto,"\n"),archivo);
         fclose(archivo);
-    }else if (strcmp(operacion, "update") == 0) {
+    /*}else if (strcmp(operacion, "update") == 0) {
         char *oldData = argv[4];
         char *newData = argv[5];
         updateRecord(ruta, oldData, newData);
     }else if (strcmp(operacion, "delete") == 0) {
         char *dataToDelete = argv[4];
         deleteRecord(ruta, dataToDelete);
-    }else {
+    */}else {
         printf("operación no válida");
     }
     
